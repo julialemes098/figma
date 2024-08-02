4d6ab01a742e42ad0c66dc5db0a3c9f559e2bfde
julialemes098 <103117367+julialemes098@users.noreply.github.com>
2024-08-02 12:06:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const botoes = document.querySelectorAll(botao);
-for(let i=0;i <botoes.length;i++) botoes[i].onclick = function() {
-
-for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(ativo);
-}
-botoes[i].classList.add(ativo);
-}
+console.log(botoes)
+for (let i=0; i < botões.length; i++) { console.log(i); }
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function() {
+    botoes[i].classList.add(ativo);
+    textos[i].classList.add("ativo");
+    textos[j].classList.remove("ativo");
+    }
+    }
+    const textos = document.querySelectorAll(".aba-conteudo");
+    const contadores = document.querySelectorAll(".contador");
+    const contadores = document.querySelectorAll(".contador");
+    const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05");
+contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = "Contagem regressiva";
