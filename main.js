2af2eb3a07cdb79a9738ea4e163b9c46e44098df
julialemes098 <103117367+julialemes098@users.noreply.github.com>
2024-08-30 11:58:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,29 @@ contadores[2].textContent = calculaTempo(tempos[2]);
 contadores[3].textContent = calculaTempo(tempos[3]);
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById("dias"+i).textContent =
+    
+    calculaTempo(tempos[i])[0];
+    
+    document.getElementById("horas"+i).textContent =
+    
+    calculaTempo(tempos[i])[1];
+    
+    document.getElementById("min"+i).textContent =
+    
+    calculaTempo(tempos[i])[2];
+    
+    document.getElementById("seg"+i).textContent =
+    
+    calculaTempo(tempos[i])[3];
+    console.log("seg"+i);
+    
+    }
+calculaTempo(tempos[1])[3];
+calculaTempo(tempos[1])[3];
+    calculaTempo(tempos[0])[3];
+    for (let i=0; i<contadores.length;i++){
+    // contadores[i].textContent = calculaTempo(tempos[i]);
     }
     }
     function comecaCronometro(){
@@ -28,8 +50,7 @@ function atualizaCronometro(){
     setInterval(atualizaCronometro,1000); }
     comecaCronometro();
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo) {
+function calculaTempo(tempoObjetivo) 
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
@@ -40,10 +61,16 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+        return [dias,horas,minutos,segundos];
+        
         } else {
-        return "Prazo Finalizado";
+        
+        return [0,0,0,0];
+        
         }
-        }
+
+
+
+
